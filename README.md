630cabf91b756f363616e0e5934a361c14f0531c
hygen <>
2020-03-17 14:44:26 -0400

    Initial oada-ensure uservice commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# oada-ensure #
+
+Ensures that things happen in other documents in response to document changes (i.e. re-indexing, copying, linking, etc.)
---
+++ index.ts
@@ -0,0 +1,13 @@
+import debug from 'debug'
+
+
+import config from './config'
+
+const info = debug('oada-ensure:info')
+const trace = debug('oada-ensure:trace')
+const warn = debug('oada-ensure:warn')
+const error = debug('oada-ensure:error')
+
+const domain = config.get('domain')
+
+// TODO: Code here
